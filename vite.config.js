1595eecbc42cee7bdb5ccfbbb3239c3c1b84d8fe
saniyasayyad <saniyasayyad4717@gmail.com>
2025-12-06 21:58:05 +0530

    manual tailwind v4 setup

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,7 @@
-import { defineConfig } from 'vite'
-<<<<<<< HEAD
-=======
-import tailwindcss from '@tailwindcss/vite'
->>>>>>> bfa1c077c0622a92f3682dbaf0c3efa4714b5db3
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
-// https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
-  plugins: [react()],
-=======
   plugins: [react(), tailwindcss()],
->>>>>>> bfa1c077c0622a92f3682dbaf0c3efa4714b5db3
-})
+});
